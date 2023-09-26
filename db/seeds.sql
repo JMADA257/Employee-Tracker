7485451bba040a03e1bd32c8391d2498109a0c1d
JMADA257 <jadams1812@hotmail.com>
2023-09-26 15:15:50 -0600

    Added boilerplate values to seeds.sql

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO departments (name)
+VALUES ("IT"),
+       ("Front-End"),
+       ("Back-End"),
+       ("Management");
+
+INSERT INTO role (title, salary)
+VALUES ("Lead engineer", 140000 ),
+       ("Senior Dev", 120000 ),
+       ("Med Dev", 90000 ),
+       ("Junior Dev", 1000 ),
+       ("CEO", 400000 ),
+       ("Director", 150000 ),
+       ("Manager", 100000 );
+       
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Jake", "Zook" ),
+       ("Maisie", "Ciullo" ),
+       ("Lillian", "E" ),
+       ("Pete", "Richards" ),
+       ("Jordan", "Adams" ),
+       ("Jason", "Borne" ),
+       ("Steven", "Rodriguez" );
+       
