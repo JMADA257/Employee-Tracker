01d1cc38806b0c0e13575388ecb618369c5dc103
JMADA257 <jadams1812@hotmail.com>
2023-09-26 15:04:53 -0600

    Updated Schema.sql with proper references

--- db/schema.sql
+++ db/schema.sql
@@ -8,11 +8,23 @@ CREATE TABLE departments (
     name VARCHAR(30) NOT NULL
 );
 
-CREATE TABLE course_names (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) NOT NULL,
-  department INT,
-  FOREIGN KEY (department)
-  REFERENCES department(id)
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES departments(id)
 );
 
+CREATE TABLE employee (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    manager_id INT,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+);
